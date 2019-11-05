b56e8fd59a08c35fe6105d2c4f36075fcc46998e
Adebayo <daramola.adebayo@yahoo.com>
2019-11-05 17:17:52 +0100

    fix(webpack): convert config object to a function

--- webpack.config.js
+++ webpack.config.js
@@ -2,47 +2,52 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 const { config } = require('dotenv');
 
-const env = config().parsed;
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+config();
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
+
+module.exports = () => {
+  const env = config().parsed;
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      },
-      {
-        test: /\.css$/,
-        loader: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    }),
-    new DefinePlugin(envKeys)
-  ],
-  devServer: {
-    historyApiFallback: true,
-  },
-  node: {
-    fs: 'empty'
-  }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader'
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          loader: ['style-loader', 'css-loader']
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: './src/index.html',
+        filename: './index.html'
+      }),
+      new DefinePlugin(envKeys)
+    ],
+    devServer: {
+      historyApiFallback: true,
+    },
+    node: {
+      fs: 'empty'
+    }
+  };
 };
